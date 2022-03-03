e5d40f998761d2c4d0c553fd1a5de62ef2c513d3
qiujingde <qiujingde@hongguaninfo.com>
2022-03-03 13:44:24 +0800

    并发demo

---
+++ lesson33/demo2/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type signal struct{}
+
+func worker(i int) {
+	fmt.Printf("worker %d: is working...\n", i)
+	time.Sleep(1 * time.Second)
+	fmt.Printf("worker %d: works done\n", i)
+}
+
+func spawnGroup(f func(int), num int, groupSignal <-chan signal) <-chan signal {
+	c := make(chan signal)
+	var wg sync.WaitGroup
+
+	wg.Add(num)
+	for i := 0; i < num; i++ {
+		go func(i int) {
+			<-groupSignal
+			fmt.Printf("worker %d: start to work...\n", i)
+			f(i)
+			wg.Done()
+		}(i + 1)
+	}
+
+	go func() {
+		wg.Wait()
+		c <- signal{}
+	}()
+
+	return c
+}
+
+func main() {
+	fmt.Println("start a group of workers...")
+	groupSignal := make(chan signal)
+	c := spawnGroup(worker, 5, groupSignal)
+	time.Sleep(5 * time.Second)
+	fmt.Println("the group of workers start to work...")
+	close(groupSignal)
+	<-c
+	fmt.Println("the group of workers work done!")
+}
